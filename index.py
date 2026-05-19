603bab966bf5aa478eba7e864b3b18ca5841ba50
jhostynelian1009 <jhostynelianb@gmail.com>
2026-05-19 17:43:35 -0500

    Actualización 2

---
+++ index.py
@@ -0,0 +1,6 @@
+_nombre_= "Jhostyn"
+_apellido_= "Baños"
+_edad_= "21"
+_cedula_= "1004596001"
+_telefono_= "0989200651"
+print("hola mi nombre es "+ _nombre_+ " " +_apellido_+ " y tengo " +_edad_+ " años con número de cedula "+_cedula_+" para servirle, le adjunto mi nro de teléfono " + _telefono_)
